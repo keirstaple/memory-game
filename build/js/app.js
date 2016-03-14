2785c0d738387edaeb203bee029b499b0355dd9e
keir staple and paul hess <keirstaple@comcast.net>
2016-03-14 14:30:23 -0700

    randomly shuffle DOM elements using a plugin from the internet

--- build/js/app.js
+++ build/js/app.js
@@ -15,7 +15,35 @@ exports.Memory = function(){
 
 var Entry = require('./../js/memory.js').Card;
 
+// Plugin pulled from http://james.padolsey.com/javascript/shuffling-the-dom/ to allow DOM shuffling//
+(function($){
+
+    $.fn.shuffle = function() {
+
+        var allElems = this.get(),
+            getRandom = function(max) {
+                return Math.floor(Math.random() * max);
+            },
+            shuffled = $.map(allElems, function(){
+                var random = getRandom(allElems.length),
+                    randEl = $(allElems[random]).clone(true)[0];
+                allElems.splice(random, 1);
+                return randEl;
+           });
+
+        this.each(function(i){
+            $(this).replaceWith($(shuffled[i]));
+        });
+
+        return $(shuffled);
+
+    };
+
+})(jQuery);
+//////////////////
+
 $(document).ready(function() {
+  $('.card').shuffle();
   $('.card').each(function() {
     $(this).click(function() {
       $(this).children('img').toggle();
